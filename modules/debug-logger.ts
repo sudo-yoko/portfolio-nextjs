c7aaab7db2b6e279585e494f32b3063dab3e1c88
sudo-yoko <it-engineer@sudou-y.com>
2025-01-20 13:32:04 +0900

    202501201331

--- modules/debug-logger.ts
+++ modules/debug-logger.ts
@@ -3,34 +3,34 @@
 /**
  * ロガーの構造定義
  */
-interface Debug {
+interface DebugLogger {
   log: typeof console.debug; // log関数にconsole.debugのシグネチャを適用
 }
 
 /**
  * 開発用のロガー実装
  */
-const development: Debug = {
+const development: DebugLogger = {
   log: (...args) => console.debug(...args),
 };
 
 /**
  * 本番用のロガー実装
  */
-const production: Debug = {
+const production: DebugLogger = {
   log: () => {}, // 空実装
 };
 
 /**
  * ロガーを生成するファクトリ
  */
-const loggerFactory = (): Debug => {
+const loggerFactory = (): DebugLogger => {
   return process.env.NODE_ENV === 'development' ? development : production;
 };
 
 /**
  * デバッグログ出力用ロガー
  */
-const debug = loggerFactory();
+const debug: DebugLogger = loggerFactory();
 
 export default debug;
