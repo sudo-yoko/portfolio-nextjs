a45889686722fbc334026d725675d5d0cea3e41a
sudo-yoko <it-engineer@sudou-y.com>
2025-01-27 02:46:31 +0900

    202501270246

--- app/sample/server-error-handling/page.tsx
+++ app/sample/server-error-handling/page.tsx
@@ -1,5 +1,5 @@
 import type { SearchParam, SearchParams } from '@/modules/types/search-params';
-import withErrorHandling from '@/modules/server-error-handler';
+import withErrorHandling from '@/modules/error-handlers/server-error-handler';
 
 export default function Page(props: { searchParams: SearchParams }) {
   return withErrorHandling(() => serverProcess());
--- modules/error-handlers/server-error-handler.ts
+++ modules/error-handlers/server-error-handler.ts
@@ -1,4 +1,4 @@
-import logger from '@/modules/logger';
+import logger from '@/modules/loggers/logger';
 
 /**
  * サーバーサイドエラーハンドリング
