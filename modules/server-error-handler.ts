5b9cce311b8535fae9ba8158c06e12e7bcfec56b
sudo-yoko <it-engineer@sudou-y.com>
2025-01-26 13:10:01 +0900

    202501261309

---
+++ modules/server-error-handler.ts
@@ -0,0 +1,24 @@
+import logger from '@/modules/logger';
+
+/**
+ * サーバーサイドエラーハンドリング
+ */
+export default async function withErrorHandling<T>(
+  func: (() => T) | (() => Promise<T>),
+): Promise<T> {
+  try {
+    // 引数に渡された関数を実行
+    const result = func();
+
+    // 引数に渡された関数が非同期関数の場合
+    if (result instanceof Promise) {
+      return await result;
+    }
+
+    // 引数に渡された関数が同期関数の場合
+    return result;
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
+}
