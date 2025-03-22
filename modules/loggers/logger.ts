bf62aac46e39b6a2ed8398e1151685011449685f
sudo-yoko <it-engineer@sudou-y.com>
2025-03-23 03:49:38 +0900

    202503230349

--- modules/loggers/logger.ts
+++ modules/loggers/logger.ts
@@ -1,10 +1,20 @@
-// アプリケーションとロギングライブラリの直接的な依存をなくし、統一的なロギングインターフェースを提供する。
+// アプリケーションとロギングライブラリの直接的な依存をなくし、
+// 統一的なロギングインターフェースを提供するロギングファサード。
 // ロギングインターフェースを実装したロガーをエクスポートする。
 
-import { loggerImpl } from '@/modules/loggers/logger-impl-winston';
 import type { ILogger } from '@/modules/loggers/logging-interface';
 import 'server-only';
 
-const logger: ILogger = loggerImpl;
+/**
+ * ロガーの実装を返す
+ */
+const provide = async (): Promise<ILogger> => {
+  return (await import('@/modules/loggers/logger-impl-winston')).loggerImpl;
+};
+
+/**
+ * ロガー実装
+ */
+const logger: ILogger = await provide();
 
 export default logger;
