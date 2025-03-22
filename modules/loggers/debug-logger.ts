3bc9c0a8393317753be8feef322b573f85a7a969
sudo-yoko <it-engineer@sudou-y.com>
2025-03-22 21:07:16 +0900

    202503222107

--- modules/loggers/debug-logger.ts
+++ modules/loggers/debug-logger.ts
@@ -1,19 +1,23 @@
-// デバッグログ出力用ロガー
+// デバッグロガーモジュール
+// 開発モードの場合にconsole.logを使用してログを出力する。
+// 本番モードの場合はロガーに空実装を適用することでログ出力を無効化する。
+// このモジュールが最初にインポートされたときにファクトリ関数が実行され、ロガーの実装が決定します。
+// 以降はキャッシュされたロガーを再利用するため効率的です。
 
 /**
- * ロガーの関数型を定義
+ * ロガーの型定義
  */
-type DebugLogger = typeof console.log;  // console.logのシグネチャを適用
+type DebugLogger = typeof console.log; // console.logのシグネチャを適用
 
 /**
  * 開発用のロガー実装
  */
-const development: DebugLogger = (...args) => console.log(...args); // コンソールログを出力
+const development: DebugLogger = (...args) => console.log('[DEBUG] ', ...args); // console.logを使用してログを出力する
 
 /**
  * 本番用のロガー実装
  */
-const production: DebugLogger = () => { };  // 空実装。本番はログ出力しない
+const production: DebugLogger = () => {}; // 空実装を適用する
 
 /**
  * ロガーを生成するファクトリ関数
@@ -23,8 +27,8 @@ const loggerFactory = (): DebugLogger => {
 };
 
 /**
- * デバッグログ出力用ロガー
+ * デバッグロガー
  */
-const debug: DebugLogger = loggerFactory();
+const debug = loggerFactory();
 
 export default debug;
