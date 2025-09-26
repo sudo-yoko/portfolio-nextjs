90e4f263c1ebc7b179347090c472981a9e6c7b37
Yoko, Sudo <83765620+sudo-yoko@users.noreply.github.com>
2025-09-26 11:28:54 +0000

    202509262028

--- app/users/mvvm/page.tsx
+++ app/users/mvvm/page.tsx
@@ -1,9 +1,1 @@
-import { Main } from '@/presentation/users/mvvm/view/main';
-
-export default function Page() {
-  return (
-    <div className="flex h-screen w-screen flex-col items-center py-10">
-      <Main />
-    </div>
-  );
-}
+export { default } from '@/presentation/users/mvvm/view/users-page.impl';
