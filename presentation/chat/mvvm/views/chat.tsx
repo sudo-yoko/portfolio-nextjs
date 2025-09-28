9fc9f0fb421172af62f6825f21943081dfd783d7
Yoko, Sudo <83765620+sudo-yoko@users.noreply.github.com>
2025-09-28 12:26:36 +0000

    202509282126

--- presentation/chat/mvvm/views/chat.tsx
+++ presentation/chat/mvvm/views/chat.tsx
@@ -17,9 +17,7 @@ export default function Chat() {
         <div className="font-bold">AI チャット</div>
         <div className="space-y-4">
           {state.chatHist?.map((chat, index) => (
-            <div key={index}>
-              {chat.role === 'user' ? 'あなた' : 'AI'}：{chat.message}
-            </div>
+            <div key={index}>{chat.role === 'user' ? <ChatYou chat={chat} /> : <ChatAI chat={chat} />}</div>
           ))}
           {state.loadingChat && <LoadingChat chat={state.loadingChat} />}
         </div>
@@ -57,9 +55,55 @@ export default function Chat() {
    * 読み込み中のAIチャットを表示するコンポーネント
    */
   function LoadingChat({ chat }: { chat: Chat }) {
+    return <div>{chat.role === 'user' ? <ChatYou chat={chat} /> : <ChatAI chat={chat} />}</div>;
+  }
+
+  function ChatYou({ chat }: { chat: Chat }) {
     return (
       <div>
-        {chat.role === 'user' ? 'あなた' : 'AI'}：{chat.message}
+        <div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+          </div>
+        </div>
+        <div>あなた：{chat.message}</div>
+      </div>
+    );
+  }
+
+  function ChatAI({ chat }: { chat: Chat }) {
+    return (
+      <div>
+        <div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+        </div>
+        <div>AI：{chat.message}</div>
       </div>
     );
   }
